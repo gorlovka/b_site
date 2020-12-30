07c0bf05b741d76fb2cbc80b2dbb1b92fe67bafa
Hans Lauer <contact@kagda.ru>
2020-12-30 23:49:56 +0300

    0 - migrations cleaned, database and table were created

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
+
     /**
      * Run the migrations.
      *
@@ -13,14 +14,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        
+        Schema::create('history',
+              function (Blueprint $table)
+        {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->float('temp');
+            $table->date('date_at');
         });
     }
 
@@ -31,6 +31,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('history');
     }
+
 }
